57ae88d063092413ec3a3c42044215a742dbf86f
Patrik Szabó <patrikszabo93@gmail.com>
2023-11-20 17:52:50 +0100

    while

---
+++ src/Iteration.java
@@ -0,0 +1,18 @@
+public class Iteration {
+    public static void main(String[] args) {
+    int number = 1;
+    while (number <=20){
+        System.out.println(number);
+        number++;
+    }
+
+
+
+
+
+
+
+
+        System.out.println("welcome");
+    }
+}
