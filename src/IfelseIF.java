48ba57533e05cbf56bd1af1ba4b034080e00690c
Patrik Szabó <patrikszabo93@gmail.com>
2023-11-20 17:42:31 +0100

    changes

---
+++ src/IfelseIF.java
@@ -0,0 +1,16 @@
+public class IfelseIF {
+    public static void main(String[] args) {
+        System.out.println("helo");
+       int housePrice = 120000;
+       if(110000 > housePrice){
+           System.out.println("i buy it");
+       }else if (housePrice > 110000 && housePrice < 130000){
+           System.out.println("could you giveit to me for 110.000");
+       }
+
+
+
+
+
+    }
+}
